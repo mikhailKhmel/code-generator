fedb2578fcff5659049ea8ba2278cc7e5a6daca2
mikhailKhmel <mikhail.khmel@gmail.com>
2021-12-13 17:37:13 +0300

    backup

--- server/routes/generator-routes.js
+++ server/routes/generator-routes.js
@@ -1,23 +1,26 @@
 const { Router } = require('express')
+const fs = require('fs')
 const path = require('path')
 const { v4: uuidv4 } = require('uuid')
 const { Generator } = require('../generator/generator')
 const router = Router()
 
 // /api/generator/run
-router.post('/run', (req, res) => {
+router.post('/run', async (req, res) => {
   try {
-    const elements = req.body.elements
-    const settings = req.body.settings
+    const json = JSON.parse(await fs.readFileSync('config\\курсовая.json', 'utf8'))
+    const elements = json.elements
+    const settings = json.settings
     const uuid = uuidv4(undefined, undefined, undefined)
-    Generator(uuid, elements, settings)
-      .then(() => {return res.download(path.resolve(__dirname, `..\\..\\projects\\archive\\${uuid}\\project.zip`))})
-      .catch((e) => {
-        return res.status(500).json({
-          message: 'Что-то пошло не так',
-          error: e.toString()
-        })
+    const { result, message } = await Generator(uuid, elements, settings)
+    if (result) {
+      return res.download(path.resolve(__dirname, `..\\..\\projects\\archive\\${uuid}\\project.zip`))
+    } else {
+      console.log(message)
+      return res.status(204).json({
+        message: message
       })
+    }
   } catch (e) {
     return res.status(500).json({ message: 'Что-то пошло не так', error: e.toString() })
   }
