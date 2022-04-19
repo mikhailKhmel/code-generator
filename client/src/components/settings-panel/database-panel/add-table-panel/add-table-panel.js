3ece373d44963fe9adffc2143f878f09b9947491
Mikhail <mikhail.khmel@gmail.com>
2022-04-19 09:34:37 +0300

    Добавлен чекбокс для заполнени случайными данными таблиц в бд

--- client/src/components/settings-panel/database-panel/add-table-panel/add-table-panel.js
+++ client/src/components/settings-panel/database-panel/add-table-panel/add-table-panel.js
@@ -3,13 +3,14 @@ import AddColumnPanel from '../add-column-panel'
 import './add-table-panel.css'
 
 export default class AddTablePanel extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       table: {
         name: '',
         columns: [],
-        foreignKeys: []
+        foreignKeys: [],
+        rndValues: false
       },
       openColumnPanel: false,
       editColumn: {}
@@ -20,22 +21,25 @@ export default class AddTablePanel extends Component {
     this.handleOpenCloseAddColumn = this.handleOpenCloseAddColumn.bind(this)
     this.handleSaveColumn = this.handleSaveColumn.bind(this)
     this.handleSaveTable = this.handleSaveTable.bind(this)
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this)
   }
 
-  componentDidMount () {
+  componentDidMount() {
     console.log('mount add-table-panel. props:', this.props)
     let table
     if (this.props.table !== undefined) {
       table = {
         name: this.props.table.name,
         columns: this.props.table.columns,
-        foreignKeys: this.props.table.foreignKeys
+        foreignKeys: this.props.table.foreignKeys,
+        rndValues: this.props.table.rndValues
       }
     } else {
       table = {
         name: '',
         columns: [],
-        foreignKeys: []
+        foreignKeys: [],
+        rndValues: false
       }
     }
     this.setState({
@@ -44,11 +48,23 @@ export default class AddTablePanel extends Component {
     })
   }
 
-  handleBackTablePanel () {
+  handleBackTablePanel() {
     this.props.onCloseAddTablePanel()
   }
 
-  handleInputChange (event) {
+  handleCheckboxChange(event) {
+    const target = event.target
+
+    const value = target.checked
+    const settingsName = target.name
+
+    this.setState((state) => {
+      state[settingsName] = value
+      return (state)
+    })
+  }
+
+  handleInputChange(event) {
     const target = event.target
 
     const value = target.value
@@ -64,11 +80,11 @@ export default class AddTablePanel extends Component {
     })
   }
 
-  handleOpenCloseAddColumn (value, editColumn) {
+  handleOpenCloseAddColumn(value, editColumn) {
     this.setState({ openColumnPanel: value, editColumn: this.state.table.columns.find(x => x.name === editColumn) })
   }
 
-  handleSaveColumn (column) {
+  handleSaveColumn(column) {
     const colIndx = this.state.table.columns.findIndex(x => x.name === column.name)
     let columns = this.state.table.columns
     if (colIndx < 0) {
@@ -86,12 +102,12 @@ export default class AddTablePanel extends Component {
     }))
   }
 
-  handleSaveTable () {
+  handleSaveTable() {
     console.log(this.props)
     this.props.onSaveTable(this.state.table)
   }
 
-  render () {
+  render() {
     console.log('render add-table-panel. state:', this.state)
 
     if (this.state.openColumnPanel) {
@@ -112,26 +128,31 @@ export default class AddTablePanel extends Component {
         </button>
 
         <input className="table-name" name="name" type="text" placeholder="Название таблицы"
-               value={this.state.table.name} onChange={this.handleInputChange}/>
+          value={this.state.table.name} onChange={this.handleInputChange} />
 
         <table className="addtablepanel-table">
           <thead>
-          <tr>
-            <td>Поля</td>
-          </tr>
+            <tr>
+              <td>Поля</td>
+            </tr>
           </thead>
           <tbody>
-          {this.state.table.columns !== undefined
-            ? this.state.table.columns.map(x => {
-              return (
-                <tr key={`${x.name}`}>
-                  <td className="column-row" onClick={() => this.handleOpenCloseAddColumn(true, x.name)}>{x.name}</td>
-                </tr>
-              )
-            })
-            : null}
+            {this.state.table.columns !== undefined
+              ? this.state.table.columns.map(x => {
+                return (
+                  <tr key={`${x.name}`}>
+                    <td className="column-row" onClick={() => this.handleOpenCloseAddColumn(true, x.name)}>{x.name}</td>
+                  </tr>
+                )
+              })
+              : null}
           </tbody>
         </table>
+        <div className="checkbox-settings">
+          <label for="rnd-values">Заполнить случайными значениями</label>
+          <input name="rnd-values" type="checkbox" onChange={this.handleCheckboxChange}
+            checked={this.state.rndValues} />
+        </div>
         <div className="btn-group">
           <button className="btn-element">Внешние ключи</button>
           <button className="btn-element" onClick={() => this.handleOpenCloseAddColumn(true)}>Добавить поле</button>
