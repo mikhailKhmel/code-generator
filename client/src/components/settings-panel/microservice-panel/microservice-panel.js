222192ed99411a31912e6b8996716007395563a5
MikhailKhmel <mikhail.khmel@gmail.com>
2022-05-10 14:56:39 +0300

    Исправление заголовка панели настроек

--- client/src/components/settings-panel/microservice-panel/microservice-panel.js
+++ client/src/components/settings-panel/microservice-panel/microservice-panel.js
@@ -5,7 +5,7 @@ import GatewayPanel from './gateway-panel'
 import './microservice-panel.css'
 
 export default class MicroservicePanel extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       settings: {
@@ -30,7 +30,7 @@ export default class MicroservicePanel extends Component {
     this.handleSaveGatewayRedirects = this.handleSaveGatewayRedirects.bind(this)
   }
 
-  handleInputChange (event) {
+  handleInputChange(event) {
     const target = event.target
 
     const value = target.type === 'checkbox' ? target.checked : target.value
@@ -48,7 +48,7 @@ export default class MicroservicePanel extends Component {
     })
   }
 
-  handleSelectChange (event) {
+  handleSelectChange(event) {
     this.setState((state) => {
       const { settings, openApiPanel } = state
       settings.microserviceType = event.target.value
@@ -59,12 +59,12 @@ export default class MicroservicePanel extends Component {
     })
   }
 
-  handleSubmit (event) {
+  handleSubmit(event) {
     event.preventDefault()
     this.props.onSaveSettings(this.state.settings)
   }
 
-  handleOpenApiPanel (event) {
+  handleOpenApiPanel(event) {
     event.preventDefault()
     this.setState(st => {
       console.log('handleOpenApi', st)
@@ -72,22 +72,22 @@ export default class MicroservicePanel extends Component {
     })
   }
 
-  handleBackApiPanel () {
+  handleBackApiPanel() {
     this.setState(st => {
       console.log('handleOpenApi', st)
       return { settings: st.settings, openApiPanel: false }
     })
   }
 
-  handleOpenGatewayPanel () {
+  handleOpenGatewayPanel() {
     this.setState({ openGatewayPanel: true })
   }
 
-  handleCloseGatewayPanel () {
+  handleCloseGatewayPanel() {
     this.setState({ openGatewayPanel: false })
   }
 
-  componentDidMount () {
+  componentDidMount() {
     console.log('mount', this.props.settings)
     const {
       id,
@@ -102,19 +102,19 @@ export default class MicroservicePanel extends Component {
     this.setState({
       openApiPanel: false,
       settings:
-        {
-          name: name === undefined ? '' : name,
-          id,
-          microserviceType:
-            microserviceType === undefined ? 'default' : microserviceType,
-          type,
-          api: api === undefined ? [] : api,
-          redirects: redirects === undefined ? [] : redirects,
-        }
+      {
+        name: name === undefined ? '' : name,
+        id,
+        microserviceType:
+          microserviceType === undefined ? 'default' : microserviceType,
+        type,
+        api: api === undefined ? [] : api,
+        redirects: redirects === undefined ? [] : redirects,
+      }
     })
   }
 
-  componentDidUpdate (prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (prevState.settings.id !== '') {
       if (this.props.settings.id !== prevState.settings.id) {
         const {
@@ -128,35 +128,35 @@ export default class MicroservicePanel extends Component {
         this.setState({
           openApiPanel: false,
           settings:
-            {
-              name: name === undefined ? '' : name,
-              id,
-              microserviceType:
-                microserviceType === undefined ? 'default' : microserviceType,
-              type,
-              api: api === undefined ? [] : api,
-              redirects: redirects === undefined ? [] : redirects
-            }
+          {
+            name: name === undefined ? '' : name,
+            id,
+            microserviceType:
+              microserviceType === undefined ? 'default' : microserviceType,
+            type,
+            api: api === undefined ? [] : api,
+            redirects: redirects === undefined ? [] : redirects
+          }
         })
       }
     }
   }
 
-  handleSaveApi (api) {
+  handleSaveApi(api) {
     console.log('handleSaveApi', api, this.state)
     this.setState(st => (
-        {
-          openApiPanel: false,
-          settings: {
-            ...st.settings,
-            api: api
-          }
+      {
+        openApiPanel: false,
+        settings: {
+          ...st.settings,
+          api: api
         }
-      )
+      }
+    )
     )
   }
 
-  handleSaveGatewayRedirects (redirects) {
+  handleSaveGatewayRedirects(redirects) {
     this.setState(st => ({
       openApiPanel: false,
       openGatewayPanel: false,
@@ -164,15 +164,15 @@ export default class MicroservicePanel extends Component {
     }))
   }
 
-  render () {
+  render() {
     if (this.state.openApiPanel) {
       return <ApiPanel onBackApiPanel={this.handleBackApiPanel} onSaveApi={this.handleSaveApi}
-                       apiData={this.state.settings.api}/>
+        apiData={this.state.settings.api} />
     }
 
     if (this.state.openGatewayPanel) {
       return <GatewayPanel redirects={this.state.settings.redirects} onCloseGatewayPanel={this.handleCloseGatewayPanel}
-                           onSaveGatewayRedirects={this.handleSaveGatewayRedirects}/>
+        onSaveGatewayRedirects={this.handleSaveGatewayRedirects} />
     }
 
     let label = ''
@@ -185,7 +185,7 @@ export default class MicroservicePanel extends Component {
         <div>
           <div className="microservice-panel">
             <div className="row">
-              <p className="object-type">Микросервис</p>
+              <p className="object-type"> {this.state.settings.microserviceType === 'default' ? 'Обычный' : 'Перенаправляющий'} Микросервис</p>
               <button
                 className="btn-close"
                 onClick={() => this.props.onCloseSettings()}
@@ -214,7 +214,7 @@ export default class MicroservicePanel extends Component {
                   Настройки перенаправления
                 </button>
             }
-            <input className="btn-save" type="submit" value="Сохранить"/>
+            <input className="btn-save" type="submit" value="Сохранить" />
           </div>
         </div>
       </form>
