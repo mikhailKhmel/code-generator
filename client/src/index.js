03c07fd722a587a22a51afcba901198877ee23d5
MKhmelnitskiy <MKhmelnitskiy@croc.ru>
2023-11-03 12:04:10 +0300

    небольшой рефакторинг

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
 import './index.css'
+import RenderFlow from './components/render-flow'
 
 ReactDOM.render(
-  <App/>,
+  <RenderFlow/>,
   document.getElementById('root')
 )
